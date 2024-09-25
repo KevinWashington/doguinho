91cfc2eb321e8ad44b78d6f4524438472a920cc6
KevinWashington <washingtonkevin703@gmail.com>
2024-09-24 22:26:58 -0300

    better hero

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,10 +1,12 @@
 <template>
     <section>
         <div class="row text-center text-md-start mb-5">
-            <div class="col-md-6 mt-3">
+            <div class="d-flex flex-column align-items-center col-md-6 mt-3 herotext">
                 <h2 class="mt-5 fs-5">Dê identidade para seu negócio</h2>
-                <h1 style="font-weight: 900;" class="display-2 fw-bold">Agência de marketing</h1>
-                <p class="text-muted mt-4">Potencialize sua presença digital com estratégias de marketing personalizadas <br> Criamos campanhas eficazes de redes sociais, SEO e conteúdo, ajudando sua marca a se conectar com o público certo e alcançar resultados concretos.</p>
+                <div>
+                    <h1 style="font-weight: 900;" class="display-2 fw-bold">Agência de marketing</h1>
+                    <p class="text-muted mt-4">Potencialize sua presença digital com estratégias de marketing personalizadas</p>
+                </div>
                 <a href="" class="btn btn-primary mt-5 w-50 p-3 fw-bold">Marcar reunião <i class="mdi mdi-arrow-right-thick"></i> </a>
             </div>
             <div class="col-md-6">
@@ -22,5 +24,11 @@ export default {
 <style>
     .nav-item .nav-link{
         color: #CA65C3 !important;
+    }
+    @media screen and (max-width: 425px) {
+        .herotext{
+            height: calc(100vh - 100px);
+            justify-content: space-evenly;
+        }
     }
 </style>
